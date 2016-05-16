3984cc67954a4b9cc03cf5dafdc8a0b5c4eb6697
Emerson Malca <emersonmalca@me.com>
2016-05-15 23:29:28 -0700

    Initial Commit

---
+++ bernievshillary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bernievshillary
+//
+//  Created by emersonmalca on 5/15/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bernievshillary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bernievshillary
+//
+//  Created by emersonmalca on 5/15/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
