66179511b4939ecb73f8e559675a6f4d4fb036b2
Emerson Malca <emersonmalca@me.com>
2016-05-17 08:32:56 -0700

    Categories for colors

---
+++ bernievshillary/UIColor+BernieVSHillary.h
@@ -0,0 +1,22 @@
+//
+//  UIColor+BernieVSHillary.h
+//  bernievshillary
+//
+//  Created by emersonmalca on 5/17/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (BernieVSHillary)
+
++ (UIColor *)bernieColor;
++ (UIColor *)hillaryColor;
++ (UIColor *)actionOrange;
++ (UIColor *)mainGreen;
++ (UIColor *)mainRed;
++ (UIColor *)navyBlue;
++ (UIColor *)lightGray;
++ (UIColor *)mediumLightGray;
+
+@end
---
+++ bernievshillary/UIColor+BernieVSHillary.m
@@ -0,0 +1,45 @@
+//
+//  UIColor+BernieVSHillary.m
+//  bernievshillary
+//
+//  Created by emersonmalca on 5/17/16.
+//
+//
+
+#import "UIColor+BernieVSHillary.h"
+
+@implementation UIColor (BernieVSHillary)
+
++ (UIColor *)bernieColor {
+    return [UIColor colorWithHue:0.5895 saturation:0.7680 brightness:0.7608 alpha:1.0];
+}
+
++ (UIColor *)hillaryColor {
+    return [UIColor colorWithHue:0.4808 saturation:0.5437 brightness:0.6275 alpha:1.0];
+}
+
++ (UIColor *)actionOrange {
+    return [UIColor colorWithHue:0.0896 saturation:0.9922 brightness:1.0000 alpha:1.0];
+}
+
++ (UIColor *)mainGreen {
+    return [UIColor colorWithHue:0.2665 saturation:0.8950 brightness:0.7098 alpha:1.0];
+}
+
++ (UIColor *)mainRed {
+    return [UIColor colorWithHue:0.9798 saturation:0.9904 brightness:0.8157 alpha:1.0];
+}
+
++ (UIColor *)navyBlue {
+    return [UIColor colorWithHue:0.5639 saturation:0.9359 brightness:0.3059 alpha:1.0];
+}
+
++ (UIColor *)lightGray {
+    return [UIColor colorWithHue:0.1042 saturation:0.0332 brightness:0.9451 alpha:1.0];
+}
+
++ (UIColor *)mediumLightGray {
+    return [UIColor colorWithHue:0.0000 saturation:0.0000 brightness:0.8471 alpha:1.0];
+}
+
+@end
